1f65642114845c292c215ec40fb76e4d358c9663
Yaju Shrestha <yajustha11@gmail.com>
2024-02-07 19:23:08 +0545

    created a table, added data using seed

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import todo
 
 # Create your views here.
 def home(request):
-  return render(request, 'index.html')
+  todo.objs = todo.objects.all()
+  data = {'todos': todo.objs}
+  return render(request, 'index.html', context = data)
